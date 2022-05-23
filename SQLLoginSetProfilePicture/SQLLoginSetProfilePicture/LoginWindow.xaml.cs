f81a269c28a9ca134e3f264afe443e431fd4422b
arjansingh00 <arjansingh00@gmail.com>
2022-05-22 19:24:52 -0700

    Add files via upload

---
+++ SQLLoginSetProfilePicture/SQLLoginSetProfilePicture/LoginWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace SQLLoginSetProfilePicture
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public bool loginFlag { get; set; }
+
+        public int UserID { get; set; }
+
+        public String userName { get; set; }
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+            loginFlag = false;
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            DataSet1TableAdapters.UsersTableAdapter userAda = new DataSet1TableAdapters.UsersTableAdapter();
+            DataTable dt = userAda.GetDataByUserAndPass(UserNameTextBox.Text, PasswordTextBox.Text);
+
+            if(dt.Rows.Count > 0)
+            {
+                //There is data
+                UserID = int.Parse(dt.Rows[0]["UserID"].ToString());
+                userName = dt.Rows[0]["UserName"].ToString();
+                loginFlag = true;
+                Close();
+            }
+            else
+            {
+                //No data
+                loginFlag = false;
+                EnterUserPasswordName.Foreground = Brushes.Red;
+                EnterUserPasswordName.Content = "Username or Password incorrect";
+            }
+        }
+
+        private void UserNameTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                if ((String.IsNullOrEmpty(UserNameTextBox.Text)) && (String.IsNullOrEmpty(PasswordTextBox.Text)))
+                {
+                    EnterUserPasswordName.Content = "Enter Username & Password";
+                }
+                else if (String.IsNullOrEmpty(PasswordTextBox.Text))
+                {
+                    EnterUserPasswordName.Content = "Enter Password";
+                }
+                else if ((String.IsNullOrEmpty(UserNameTextBox.Text)))
+                {
+                    EnterUserPasswordName.Content = "Enter Username";
+                }
+                else
+                {
+                    Login_Click(sender, e);
+                }
+            }
+        }
+
+        private void PasswordTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                if ((String.IsNullOrEmpty(UserNameTextBox.Text)) && (String.IsNullOrEmpty(PasswordTextBox.Text)))
+                {
+                    EnterUserPasswordName.Content = "Enter Username & Password";
+                }
+                else if (String.IsNullOrEmpty(PasswordTextBox.Text))
+                {
+                    EnterUserPasswordName.Content = "Enter Password";
+                }
+                else if ((String.IsNullOrEmpty(UserNameTextBox.Text)))
+                {
+                    EnterUserPasswordName.Content = "Enter Username";
+                }
+                else
+                {
+                    Login_Click(sender, e);
+                }
+            }
+        }
+    }
+}
---
+++ SQLLoginSetProfilePicture/SQLLoginSetProfilePicture/MainWindow.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Windows.Interop;
+using Microsoft.Win32;
+using Path = System.IO.Path;
+
+namespace SQLLoginSetProfilePicture
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int loggedIn { get; set; }
+
+        public int UserID { get; set; }
+
+        public String userName { get; set; }
+
+        public String ImageSelectorSelectedItem { get; set; }
+
+        public String fileName { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataSet1TableAdapters.ImageTableTableAdapter imgada = new DataSet1TableAdapters.ImageTableTableAdapter();
+
+            DataTable dataTable;
+
+            try
+            {
+                dataTable = imgada.GetSingleImage(ImageSelector.SelectedItem.ToString());
+            }
+            catch(NullReferenceException)
+            {
+                dataTable = imgada.GetSingleImage(fileName);
+            }
+
+            if (dataTable.Rows.Count > 0)
+            {
+                DataRow pictureRow = dataTable.Rows[0];
+                if (pictureRow[2] != System.DBNull.Value)
+                {
+                    byte[] imageData = (byte[])pictureRow[2];
+                    System.Drawing.Image imageFromBinary = ConvertBinaryToImage(imageData);
+                    var bitmap = new System.Drawing.Bitmap(imageFromBinary);
+                    var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                    bitmap.Dispose();
+                    var brush = new ImageBrush(bitmapSource);
+                    ImageCircle.Fill = brush;
+                }
+                else
+                {
+                    ImageCircle.Fill = null;
+                }
+            }
+        }
+
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+            userName = loginWindow.userName;
+
+            ImageSelector.SelectedIndex = 1;
+
+            if (loginWindow.loginFlag == false)
+            {
+                System.Windows.Application.Current.Shutdown();
+            }
+
+            UserName.Content = userName;
+
+            DataSet1TableAdapters.ImageTableTableAdapter imgada = new DataSet1TableAdapters.ImageTableTableAdapter();
+
+            DataTable dataTable = imgada.GetImageData(userName);
+
+            foreach(DataRow rows in dataTable.Rows)
+            {
+                ImageSelector.Items.Add(rows["FileName"].ToString());
+            }
+
+            if (dataTable.Rows.Count > 0)
+            {
+                DataRow pictureRow = dataTable.Rows[0];
+                if (pictureRow[2] != System.DBNull.Value)
+                {
+                    byte[] imageData = (byte[])pictureRow[2];
+                    System.Drawing.Image imageFromBinary = ConvertBinaryToImage(imageData);
+                    var bitmap = new System.Drawing.Bitmap(imageFromBinary);
+                    var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                    bitmap.Dispose();
+                    var brush = new ImageBrush(bitmapSource);
+                    ImageCircle.Fill = brush;
+                }
+                else
+                {
+                    ImageCircle.Fill = null;
+                }
+            }
+        }
+
+        System.Drawing.Image ConvertBinaryToImage(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                return System.Drawing.Image.FromStream(ms);
+            }
+        }
+
+        byte[] ConvertImageToBinary(System.Drawing.Image img)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
+        }
+
+        private void Upload_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false };
+
+            if(ofd.ShowDialog() == true)
+            {
+                ImageBrush brush = new ImageBrush();
+                brush.ImageSource = new BitmapImage(new Uri(ofd.FileName));
+                ImageCircle.Fill = brush;
+
+                Bitmap image = new Bitmap(ofd.FileName);
+
+                Byte[] imgData = ConvertImageToBinary(image);
+
+                DataSet1TableAdapters.ImageTableTableAdapter imgUpload = new DataSet1TableAdapters.ImageTableTableAdapter();
+
+                fileName = Path.GetFileName(ofd.FileName);
+                imgUpload.AddImage(userName, imgData, fileName);
+                ImageSelector.Items.Add(fileName);
+                ImageSelector.SelectedIndex = ImageSelector.Items.IndexOf(fileName);
+            }
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false };
+
+            if (ofd.ShowDialog() == true)
+            {
+                ImageBrush brush = new ImageBrush();
+                brush.ImageSource = new BitmapImage(new Uri(ofd.FileName));
+                ImageCircle.Fill = brush;
+
+                Bitmap image = new Bitmap(ofd.FileName);
+
+                Byte[] imgData = ConvertImageToBinary(image);
+
+                DataSet1TableAdapters.ImageTableTableAdapter imgUpload = new DataSet1TableAdapters.ImageTableTableAdapter();
+
+                fileName = Path.GetFileName(ofd.FileName);
+
+                imgUpload.UpdateImage(imgData, fileName); // Updates record with new image data and file name
+                ImageSelector.SelectedItem = fileName;
+
+            }
+        }
+    }
+}
